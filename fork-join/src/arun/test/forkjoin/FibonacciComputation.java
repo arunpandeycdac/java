94870c28807df5cc9eaf1692fcc22d414dd82587
Arun Pandey <arunpandeycdac@gmail.com>
2017-09-11 14:58:19 -0400

    ForkJoinTest
    This is to see the Java7 feature of Forkjoin

---
+++ fork-join/src/arun/test/forkjoin/FibonacciComputation.java
@@ -0,0 +1,31 @@
+package arun.test.forkjoin;
+
+import java.util.concurrent.RecursiveTask;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author arun.pandey
+ */
+public class FibonacciComputation extends RecursiveTask<Integer> {
+	
+	private static final long serialVersionUID = 1L;
+	public static final Log LOG = LogFactory.getLog(FibonacciComputation.class);
+	private final int number;
+	
+	public FibonacciComputation(int number) { 
+		this.number = number; 
+	}
+
+	@Override
+	public Integer compute() {
+		if (number <= 1)
+			return number;
+		FibonacciComputation f1 = new FibonacciComputation(number - 1);
+		f1.fork();
+		LOG.info("Current Therad Name = "+Thread.currentThread().getName());
+		FibonacciComputation f2 = new FibonacciComputation(number - 2);
+		return f2.compute() + (Integer)f1.join();
+	}
+}
